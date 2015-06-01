ddd644e8ace6fa5c668a58a5bc0d07f8b00fe5d7
Grondwortel <leonpinguin@hotmail.com>
2015-06-01 09:15:46 +0100

    he

---
+++ contact.php
@@ -0,0 +1,33 @@
+
+
+    <!-- Projecten -->
+    <section id="portfolio" class="bg-light-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Contact</h2>
+                    <h3 class="section-subheading text-muted">Waar kan je me allemaal op bereiken</h3>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 portfolio-item">
+                        <a href=".php" class="portfolio-link" data-toggle="modal">
+                            <div class="portfolio-hover">
+                                <div class="portfolio-hover-content">
+                                    <i class="fa fa-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img src="img/portfolio/leonjpg.jpg" class="img-responsive" alt="">
+                        </a>
+                        <div class="portfolio-caption">
+                            <h4>Een foto van mij</h4>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    </html>
---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+
+    <title>Portfolio Léon</title>
+
+    <!-- Alle CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link href="css/agency.css" rel="stylesheet">
+
+
+
+</head>
+
+
+
+
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Welkom bij mijn portfolio!</div>
+                <div class="intro-heading">Léon Niesthoven</div>
+
+            </div>
+        </div>
+    </header>
+
+    <?php
+    include 'watikkan.html';
+    ?>
+
+    <?php
+    include 'portfolio.html';
+    ?>
+
+    <?php
+    include 'contact.php';
+    ?>
